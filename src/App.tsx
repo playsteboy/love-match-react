faada776a6af37daa8e3adcdaed59a7f934a45e4
Fiantso Harena <fiantsorav@gmail.com>
2025-07-31 07:43:28 +0300

    chore: say hello to k3

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <main className="w-full h-screen flex items-center justify-center">
+      <h1 className="text-5xl font-bold">Hello les K3 :)</h1>
+    </main>
+  );
+}
