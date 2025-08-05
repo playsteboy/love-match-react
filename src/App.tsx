0e6a3f74866d9bb66127152dc7052c8fdf3c3ac7
Fiantso Harena <fiantsorav@gmail.com>
2025-08-05 20:59:26 +0300

    style: use gsap for text animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { SplitText } from './components';
+
 export default function App() {
   return (
     <main className="w-full h-screen flex items-center justify-center">
-      <h1 className="text-5xl font-bold">Hello les K3 :)</h1>
+      <SplitText
+        text="Hello les K3 :)"
+        className="text-5xl font-semibold text-center"
+        delay={100}
+        duration={0.6}
+        ease="power3.out"
+        splitType="chars"
+        from={{ opacity: 0, y: 40 }}
+        to={{ opacity: 1, y: 0 }}
+        threshold={0.1}
+        rootMargin="-100px"
+        textAlign="center"
+      />
     </main>
   );
 }
